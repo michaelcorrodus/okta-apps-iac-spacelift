a987d827d6bb2b2081ccf298ba0a710dfbdd5358
michaelcorrodus <admin@iamconnect.co.uk>
2025-11-12 17:23:46 +0000

    Update variables.tf

--- infra/modules/okta-app-spa-oidc/variables.tf
+++ infra/modules/okta-app-spa-oidc/variables.tf
@@ -1,4 +1,17 @@
-variable "label" { type = string }
-variable "redirect_uris" { type = list(string) }
-variable "group_names" { type = list(string) default = [] }
-variable "group_ids" { type = list(string) default = [] }
+variable "label" {
+  type = string
+}
+
+variable "redirect_uris" {
+  type = list(string)
+}
+
+variable "group_names" {
+  type    = list(string)
+  default = []
+}
+
+variable "group_ids" {
+  type    = list(string)
+  default = []
+}
