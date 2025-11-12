1d5571340ca33d10859d3656acfadd4bcfed2395
michaelcorrodus <admin@iamconnect.co.uk>
2025-11-12 17:45:01 +0000

    Update versions.tf

--- infra/versions.tf
+++ infra/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 1.5.0"
   required_providers {
     okta = {
-      source  = "okta/okta"
-      version = "= 6.3.1"   # <- pin away from 6.4.0 which crashed
+      source  = "hashicorp/okta"
+      version = "= 6.3.1"
     }
   }
 }
