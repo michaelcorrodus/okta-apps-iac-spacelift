d36eb1d4cccc08da19ce6a620d70246c51788845
michaelcorrodus <104002901+michaelcorrodus@users.noreply.github.com>
2025-11-13 00:27:11 +0000

    deletesalesforceapp

--- infra/salesforcetestapp2.tf
+++
@@ -1,8 +0,0 @@
-# Test app deployment
-
-module "salesforcetest2_spa_app" {
-  source        = "./modules/okta-app-spa-oidc"
-  label         = "Salesforce test 2 app "
-  redirect_uris = ["https://test.ft.com/callback"]
-  group_names   = ["Iamconnect_Salesforcenew_Users"]
-}
